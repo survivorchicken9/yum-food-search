1658c07e1858663f08c216bfa0cfe6fed6962e80
Nicole Ip <nicoleapril29@gmail.com>
2021-04-12 20:40:16 -0400

    installed react tt-router

--- web/app/src/App.js
+++ web/app/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          byroncooks
+          <h1>Filipino Food</h1>
+          <h3>for the homesick and hungry</h3>
+        </header>
+      </div>
+    </Router>
   );
 }
 
